4102b8459743c3e24de6595b174035f259097299
Mustafa Burak AYDIN <mburak.aydin@xenonsmart.com>
2026-02-16 11:57:24 +0000

    pipeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.ENV_FILE || '/mnt/secrets/.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    const file = process.env.HELLO_FILE || '/mnt/secrets/env';
-    try {
-      return fs.readFileSync(file, 'utf8').trim();
-    } catch {
-      return 'PRIVATE';
-    }
+    return process.env.HELLO ?? 'YA OL DA';
   }
 }
