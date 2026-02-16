678dd5b1c68a3084ff3deb3acac60c64d858de8e
Mustafa Burak AYDIN <mburak.aydin@xenonsmart.com>
2026-02-16 10:58:22 +0000

    added file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'PRIVATE';
+    const file = process.env.HELLO_FILE || '/mnt/secrets/env';
+    try {
+      return fs.readFileSync(file, 'utf8').trim();
+    } catch {
+      return 'PRIVATE';
+    }
   }
 }
