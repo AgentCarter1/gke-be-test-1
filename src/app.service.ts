8905e34ba91f5841630494bed1a3c82a20ac93c9
Mustafa Burak Aydın <mburak.aydin@xenonsmart.com>
2026-02-18 11:24:27 +0300

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `${process.env.PORT} Guncellendi YAML` ;
+    return `${process.env.PORT} Guncellendi YAML GTIOPS` ;
   }
 }
